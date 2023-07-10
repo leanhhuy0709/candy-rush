f95d7b0d39cae0bfc20368f54928c1c4cca6be80
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-07-10 17:26:07 +0700

    Add assets

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Candy Rush',
+    url: 'https://github.com/leanhhuy0709/candy-rush',
+    version: '2.0',
+    type: Phaser.AUTO,
+    parent: 'Game',
+    scene: [
+    ],
+    input: {
+        keyboard: true,
+    },
+    width: 520,
+    height: 700,
+    scale: {
+        autoCenter: Phaser.Scale.Center.CENTER_BOTH
+    },
+    backgroundColor: '#123456',
+    render: { pixelArt: false, antialias: true }
+}
